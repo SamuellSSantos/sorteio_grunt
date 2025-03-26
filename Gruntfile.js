b8578c905473f05a8e9a98fbe4670cb2fec3dfcd
Samuel Silva dos Santos <akitadecumace@gmail.com>
2025-03-26 08:44:37 -0300

    atualização de projeto

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,10 @@ module.exports = function(grunt) {
                         match: 'ENDERECO_DO_CSS',
                         replacement: './styles/main.min.css'
 
+                    },
+                    {
+                        match: 'ENDERECO_DO_JS',
+                        replacement: '.scripts/main.min.js'
                     }
                 ]
         },
@@ -84,7 +88,14 @@ module.exports = function(grunt) {
             }
         }
     },
-    clean: ['prebuild']
+    clean: ['prebuild'],
+    uglify: {
+        target: {
+            files: {
+                'dist/scripts/main.min.js': 'src/scripts/main.js'
+            }
+        }
+    }
     }); 
 
     grunt.loadNpmTasks('grunt-contrib-less');
@@ -92,7 +103,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:development', 'htmlmin:dist', 'replace:dev', 'clean']);
+    grunt.registerTask('build', ['less:development', 'htmlmin:dist', 'replace:dev', 'clean', 'uglify']);
 }
